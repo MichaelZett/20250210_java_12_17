2780f53bf5f8c3b10fdc00fe77adad816a0f1047
Michael Zöller <michael2.zoeller@gmail.com>
2025-02-10 08:03:15 +0100

    Remove solution for now

--- 02_java12_17/src/main/java/de/zettsystems/solution12_15/Season.java
+++
@@ -1,5 +0,0 @@
-package de.zettsystems.solution12_15;
-
-public enum Season {
-    SPRING, SUMMER, AUTUMN, WINTER
-}
--- 02_java12_17/src/main/java/de/zettsystems/solution16_17/FifoQueueStrategyImpl.java
+++
@@ -1,8 +0,0 @@
-package de.zettsystems.solution16_17;
-
-public final class FifoQueueStrategyImpl implements QueueStrategy {
-    @Override
-    public void execute() {
-        // first in, first out
-    }
-}
